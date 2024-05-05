26a5b1ffc0900e41bd6287c9545cbbbe4c183e47
davidkac <david.drinic9@yahoo.com>
2024-05-05 22:22:48 +0200

    improve UI on login and register pages

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,38 +3,41 @@
 @section('title', 'Login')
 
 @section('content')
-
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
-
-        <div>
-            <label for="email">Email:</label>
-            <input type="text" name="email" id="email" value="{{ old('email') }}" autofocus>
-            @error('email')
-                <div>{{ $message }}</div>
-            @enderror
+    <div class="row">
+        <div class="col-md-6 offset-md-3">
+            <div class="card mt-5">
+                <div class="card-body">
+                    <form method="POST" action="{{ route('login') }}">
+                        @csrf
+                        
+                        @error('invalidCredentials')
+                            <div class="mb-3 text-center text-danger">{{ $message }}</div>
+                        @enderror
+                        
+                        <div class="mb-3">
+                            <label for="email" class="form-label">Email</label>
+                            <input type="text" class="form-control" id="email" name="email" value="{{ old('email') }}" autofocus>
+                            @error('email')
+                                <div>{{ $message }}</div>
+                            @enderror
+                        </div>
+                        <div class="mb-3">
+                            <label for="password" class="form-label">Password</label>
+                            <input type="password" class="form-control" id="password" name="password">
+                            @error('password')
+                                <div>{{ $message }}</div>
+                            @enderror
+                        </div>
+                        <div class="mb-3 text-center">
+                            If you don't have an account yet, you can create one
+                            <a href="{{ route('register') }}">here</a>.
+                        </div>
+                        <div class="text-end mt-4">
+                            <button type="submit" class="btn btn-primary">Login</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
-
-        <div>
-            <label for="password">Password:</label>
-            <input type="password" name="password" id="password">
-            @error('password')
-                <div>{{ $message }}</div>
-            @enderror
-        </div>
-
-        <div class="my-2">
-            If you don't have an account yet, you can create one
-            <a href="{{ route('register') }}">here</a>.
-        </div>
-        
-        <div>
-            <button type="submit">Login</button>
-        </div>
-    </form>
-
-    @error('invalidCredentials')
-        <div>{{ $message }}</div>
-    @enderror
-
+    </div>
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,41 +3,43 @@
 @section('title', 'Register')
 
 @section('content')
-    <form method="POST" action="{{ route('register') }}">
-        @csrf
-
-        <div>
-            <label for="name">Name:</label>
-            <input type="text" name="name" id="name" value="{{ old('name') }}" autofocus>
-            @error('name')
-                <div>{{ $message }}</div>
-            @enderror
+    <div class="row">
+        <div class="col-md-6 offset-md-3">
+            <div class="card mt-5">
+                <div class="card-body">
+                    <form method="POST" action="{{ route('register') }}">
+                        @csrf
+                        <div class="mb-3">
+                            <label for="name" class="form-label">Name</label>
+                            <input type="text" class="form-control" id="name" name="name" value="{{ old('name') }}" autofocus>
+                            @error('name')
+                                <div>{{ $message }}</div>
+                            @enderror
+                        </div>
+                        <div class="mb-3">
+                            <label for="email" class="form-label">Email</label>
+                            <input type="text" class="form-control" id="email" name="email" value="{{ old('email') }}">
+                            @error('email')
+                                <div>{{ $message }}</div>
+                            @enderror
+                        </div>
+                        <div class="mb-3">
+                            <label for="password" class="form-label">Password</label>
+                            <input type="password" class="form-control" id="password" name="password">
+                            @error('password')
+                                <div>{{ $message }}</div>
+                            @enderror
+                        </div>
+                        <div class="mb-3">
+                            <label for="password_confirmation" class="form-label">Confirm Password</label>
+                            <input type="password" class="form-control" id="password_confirmation" name="password_confirmation">
+                        </div>
+                        <div class="text-end mt-4">
+                            <button type="submit" class="btn btn-primary">Register</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
-
-        <div>
-            <label for="email">Email:</label>
-            <input type="text" name="email" id="email" value="{{ old('email') }}">
-            @error('email')
-                <div>{{ $message }}</div>
-            @enderror
-        </div>
-
-        <div>
-            <label for="password">Password:</label>
-            <input type="password" name="password" id="password">
-            @error('password')
-                <div>{{ $message }}</div>
-            @enderror
-        </div>
-
-        <div>
-            <label for="password_confirmation">Confirm Password:</label>
-            <input type="password" name="password_confirmation" id="password_confirmation">
-        </div>
-
-        <div>
-            <button type="submit">Register</button>
-        </div>
-    </form>
-
+    </div>
 @endsection
